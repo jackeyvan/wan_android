4107cac9e510aa4bc5fe316bd7ad3390e052f283
alex <xmpinger@gmail.com>
2024-08-15 00:42:55 +0800

    clean project

--- wan_android_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ wan_android_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,14 +5,12 @@
 import FlutterMacOS
 import Foundation
 
-import connectivity_plus
 import flutter_inappwebview_macos
 import path_provider_foundation
 import sqflite
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
