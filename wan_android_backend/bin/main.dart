616441f2989965b7d00e61cb1018828fb8ccf546
alex <xmpinger@gmail.com>
2024-08-18 01:32:21 +0800

    update project

--- wan_android_backend/bin/main.dart
+++ wan_android_backend/bin/main.dart
@@ -5,6 +5,7 @@ import 'package:shelf/shelf_io.dart';
 import 'package:wan_android_backend/db/hive_box.dart';
 import 'package:wan_android_backend/route/middleware.dart';
 import 'package:wan_android_backend/route/route_handler.dart';
+import 'package:wan_android_backend/route/shelf_cors.dart';
 import 'package:wan_android_backend/schedule/schedule.dart';
 
 void main(List<String> args) async {
@@ -19,16 +20,13 @@ void main(List<String> args) async {
   final handler = Pipeline()
       .addMiddleware(logRequests())
       .addMiddleware(rejectBadRequests())
+      .addMiddleware(corsMiddleware())
       .addHandler(RouteHandler().router.call);
 
   final ip = InternetAddress.anyIPv4;
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
   final server = await serve(handler, ip, port);
   server.autoCompress = true;
-
-  defaultResponseHeaders.forEach((key, value) {
-    server.defaultResponseHeaders.add(key, value);
-  });
 
   print('Server run success, listening on port ${server.port}');
   print("http://${ip.address}:${server.port}/api/v1/banner");
