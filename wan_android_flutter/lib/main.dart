989efaa510ed3ae910c7480dafab9fcf63d630d0
shacoc <xmpinger@gmail.com>
2024-08-19 18:40:51 +0800

    fixed web h5 ui

--- wan_android_flutter/lib/main.dart
+++ wan_android_flutter/lib/main.dart
@@ -17,6 +17,7 @@ void main() async {
 class WanApp extends StatelessWidget {
   const WanApp({super.key});
 
+  static const _mobileWidthThreshold = 500;
   static const _mobileWidth = 420.0;
   static const _mobileHeight = 900.0;
 
@@ -32,32 +33,35 @@ class WanApp extends StatelessWidget {
       translations: Languages(),
       locale: Languages.locale,
       fallbackLocale: Languages.fallbackLocale,
-      builder: (context, widget) {
-        if (GetPlatform.isWeb) {
-          return _buildFrame(widget!);
-        }
-        return widget!;
-      },
+      builder: (context, widget) => _buildFrame(context, widget!),
     );
   }
 
   /// 在Web上展示Mobile的样式
-  Widget _buildFrame(Widget widget) {
-    return Builder(builder: (context) {
-      return Scaffold(
-          backgroundColor: Theme.of(context).colorScheme.onInverseSurface,
-          body: Center(
-            child: Card(
-              elevation: 10,
-              margin: const EdgeInsets.symmetric(vertical: 16),
-              clipBehavior: Clip.hardEdge,
-              child: SizedBox(
-                height: _mobileHeight,
-                width: _mobileWidth,
-                child: widget,
-              ),
-            ),
-          ));
-    });
+  Widget _buildFrame(BuildContext context, Widget widget) {
+    if (GetPlatform.isWeb) {
+      final window = View.of(context);
+      final size = window.physicalSize / window.devicePixelRatio;
+      final hasFrame = size.width > _mobileWidthThreshold;
+      if (hasFrame) {
+        return Builder(builder: (context) {
+          return Scaffold(
+              backgroundColor: Theme.of(context).colorScheme.onInverseSurface,
+              body: Center(
+                child: Card(
+                  elevation: 10,
+                  margin: const EdgeInsets.symmetric(vertical: 16),
+                  clipBehavior: Clip.hardEdge,
+                  child: SizedBox(
+                    height: _mobileHeight,
+                    width: _mobileWidth,
+                    child: widget,
+                  ),
+                ),
+              ));
+        });
+      }
+    }
+    return widget;
   }
 }
