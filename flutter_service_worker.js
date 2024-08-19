55228343c8e68bd98cf5c7099f41de67f7d87aeb
jackeyvan <jackeyvan@users.noreply.github.com>
2024-08-19 17:05:02 +0000

    deploy: 66f3ebdfefceb7f33497faab69ed03789b093b2f

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,7 +28,7 @@ const RESOURCES = {"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb0489
 "main.dart.js": "fc128ea6a1a3c1dc13e2788de7ffff5c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "4c4b230b1397f22913ce032466cadf78",
-"flutter_bootstrap.js": "8b0f134e155acfd0602fc74945ce36d9",
+"flutter_bootstrap.js": "49db92ca06ba6599792990f36df5d17c",
 "canvaskit/skwasm.js.symbols": "262f4827a1317abb59d71d6c587a93e2",
 "canvaskit/skwasm.wasm": "9f0c0c02b82a910d12ce0543ec130e60",
 "canvaskit/chromium/canvaskit.wasm": "b1ac05b29c127d86df4bcfbf50dd902a",
